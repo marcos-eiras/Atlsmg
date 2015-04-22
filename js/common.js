78608cdb7e2198a0df2e5d7a20ffee856fdd1bdb
ricardo <ricardo@ricardosierra>
2015-04-21 22:57:43 -0300

    Erro no js

--- js/common.js
+++ js/common.js
@@ -68,6 +68,7 @@ jQuery(function() {
 });
 
 
+
 var SierraTecnologia = (function () {
     "use strict";
     // VARIAVEIS PRIMARIAS
@@ -86,4 +87,152 @@ var SierraTecnologia = (function () {
     }
     
     function Controle_Menu_Cursos(){ 
-        $('.menucursos
+        $('.menucursos').append(Visual_Menu_Cursos(Modelo('Curso')));
+    }    
+    
+    function Controle_Turma_Listagem(Curso){ 
+        $('#conteudo').append(Visual_Turma_Listagem(Modelo('Turma&id='+Curso)));
+    }
+    
+    
+    
+    function Modelo(tipo){
+        var resultado = false;
+        $.ajax({
+            // url para o arquivo json.php
+            url : "index.php?TemaTipo="+tipo,
+            async: false,
+            // dataType json
+            dataType : "json",
+            // função para de sucesso
+            success : function(data){
+            // vamos gerar um html e guardar nesta variável
+                resultado = data;
+                return true;
+            }
+        });//termina o ajax
+        return resultado;
+    }
+
+    function Visual_Menu_Cursos(data){
+        var html = "";
+        var i;
+        
+        if(data===false) return '';
+
+        for(i=0; i < data.length; i++){
+            
+             html = html+"<li class=parent><a href=Lista_Turmas_Curso.php?id_curso="+data[i].id+">"+data[i].nome+"</a></li><br><br>";
+        }
+        
+        return html;
+    }
+
+    function Visual_Home_Cursos(data){
+        var html = "";
+        var i;
+        
+        if(data===false) return '';
+
+        for(i=0; i < data.length; i++){
+            html = html+"<div class=\"block-01\"><p><span style=\"color:#08c; font-size:22px;\">"+data[i].nome+"</span><br>"+data[i].descricao+"</p><br><p><span style=\"color:#08c;\">Requisitos:</span> "+data[i].requisito+"<br><span style=\"color:#08c;\">Valor:</span> "+Converter_Float_Real(data[i].valor)+"</p></div>";
+        }
+        
+        return html;
+    }
+    
+    function Visual_Turma_Listagem(data){
+        var html = "";
+        var i;
+        
+        if(data===false) return '';
+
+        for(i=0; i < data.length; i++){
+            
+             html = html+
+                ""+
+                data[i].id+
+                ""+
+                data[i].nome+
+                ""+
+                data[i].qnt+
+                ""+
+                data[i].inicio+
+                ""+
+                data[i].fim+
+                ""+
+                data[i].carga+
+                ""+
+                data[i].descricao+
+                "<br><br>";
+        }
+        
+        console.log('Chamou');
+        return html;
+    }
+    
+    function Atualizar(){
+        
+    }
+    
+    function Converter_Real_Float(valor) {
+        valor = valor.replace("R$ ","");
+        valor = valor.replace(".","");
+        valor = valor.replace(",",".");
+        valor = parseFloat(valor);
+        return valor;
+    }
+    function Converter_Float_Real(valor) {
+        var inteiro = null, 
+            decimal = null, 
+            c = null, 
+            j = null,
+            aux = new Array();
+        valor = ""+valor;
+        c = valor.indexOf(".",0);
+        //encontrou o ponto na string
+        if (c > 0) {
+           //separa as partes em inteiro e decimal
+           inteiro = valor.substring(0,c);
+           decimal = valor.substring(c+1,valor.length);
+        }else{
+           inteiro = valor;
+        }
+
+        //pega a parte inteiro de 3 em 3 partes
+        for (j = inteiro.length, c = 0; j > 0; j-=3, c++) {
+           aux[c]=inteiro.substring(j-3,j);
+        }
+
+        //percorre a string acrescentando os pontos
+        inteiro = "";
+        for (c = aux.length-1; c >= 0; c--) {
+           inteiro += aux[c]+'.';
+        }
+        //retirando o ultimo ponto e finalizando a parte inteiro
+
+        inteiro = inteiro.substring(0,inteiro.length-1);
+
+        decimal = parseInt(decimal);
+        if (isNaN(decimal)) {
+           decimal = "00";
+        }else{
+           decimal = ""+decimal;
+           if (decimal.length === 1) {
+              decimal = decimal+"0";
+           }
+        }
+
+
+        valor = "R$ "+inteiro+","+decimal;
+
+
+        return valor;
+    }
+    
+    Controle();
+        
+    return {
+        Controle: Controle,
+    }
+}());
